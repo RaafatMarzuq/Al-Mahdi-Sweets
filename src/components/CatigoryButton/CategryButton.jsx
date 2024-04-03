b3bd719da7a873f8f2064d6c6e54f2367fb79df2
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2024-04-03 11:52:58 +0300

    make it responsive

--- src/components/CatigoryButton/CategryButton.jsx
+++ src/components/CatigoryButton/CategryButton.jsx
@@ -4,8 +4,8 @@ import './style.css';
 const CatigryButton = (props) => {
 
   return (
-      <div className="imageButton" onClick={()=>{ props.onClick(props.id)}} style={{ width: '30%' }}>
-        <span className="imageSrc" style={{ backgroundImage: "url('//Al-Mahdi-Sweets/images/" + props.ImageURL + "')"}}></span>
+      <div className="imageButton" onClick={()=>{ props.onClick(props.id)}} style={{ width: '25%' }}>
+        <span className="imageSrc" style={{ backgroundImage: "url('Al-Mahdi-Sweets/images/" + props.ImageURL + "')"}}></span>
         <span className="imageBackdrop"></span>
         <span className="image">
           <span className="imageText">
