22c61b62660c6b4ff58c7abc5cb9176eeaacc2d4
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2024-04-15 21:55:04 +0300

    adding pop up message

--- src/components/PopUpMessage/PopUpMessage.jsx
+++ src/components/PopUpMessage/PopUpMessage.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './popUpStyle.css'
+import React, { useEffect, useState } from 'react';
+import './popUpStyle.css';
+import  todaysSpecial  from './knafe.png';
+
 
 const Popup = ({ message, onClose }) => {
+  const [topOffset, setTopOffset] = useState(300);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const popupHeight = document.querySelector('.popup').offsetHeight;
+      const popupTopOffset = (windowHeight - popupHeight) / 2 + window.scrollY;
+      setTopOffset(popupTopOffset);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="popup">
+    <div className="popup" style={{ top: `${topOffset}px` }}>
       <div className="popup-inner">
         <button className="close-btn" onClick={onClose}>סגור</button>
-        <p>המיוחדים שלנו להיום</p>
+        <h1>המיוחדים שלנו להיום</h1>
+        <img className='todays-speacial-image' src={todaysSpecial} alt='knafe' />
         <p>{message}</p>
       </div>
     </div>
