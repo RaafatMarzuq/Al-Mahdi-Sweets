7cf677cd167b2eece5618977e57bef8dab3d8b6f
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2024-04-16 22:38:30 +0300

    adding go back button to manu page

--- src/pages/CoffeePage/MenuPage.jsx
+++ src/pages/CoffeePage/MenuPage.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
+import { MdOutlineArrowBack } from "react-icons/md";
+
 import './style.css';
 
 const MenuPage = (props) => {
   
   return (
     <div className="menu-container">
-      
       <ul className="menu-list">
-      <h1>{props.MenuName}</h1>
+        <Link to="/" className="back-button"><MdOutlineArrowBack style={{ fontSize: '2rem' }} /></Link>
+
+        <h1>{props.MenuName}</h1>
         {props.MenuItems.map((item, index) => (
           <li key={index} className="menu-item">
             <span className="item-price">{item.price}</span>
-             <span className="item-name">{item.name}</span>
+            <span className="item-name">{item.name}</span>
           </li>
         ))}
       </ul>
